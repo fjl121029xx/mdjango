fe9226b4bbfd84af8b1d39541e7ed0298d35313e
fjl121029xx <sishengqikuo_xx@163.com>
2021-01-07 16:26:20 +0800

    django demo

---
+++ odsweb/views.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2021/1/7 15:51
+__author__ = 'the king of north'
+
+from django.shortcuts import render
+
+
+def runoob(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'runoob.html', context)
+
+
+def runoob2(request):
+    context = {}
+    context['hello'] = 'Hello Worl2d!++'
+    return render(request, 'runoob2.html', context)
+
+
+def runoob_list(request):
+    views_list = ["菜鸟教程1", "菜鸟教程2", "菜鸟教程3"]
+    return render(request, "runoob2.html", {"views_list": views_list})
